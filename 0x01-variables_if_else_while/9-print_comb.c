a30cf5ec3c964e2cac0725abee23ead0fca1dadf
noceedaleighn <no.ceepho06@gmail.com>
2023-03-19 02:58:52 -0700

    Variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'print alphabets lowercase and uppercase'
- * Return: always 0
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		putchar(',');
-		putchar(' ');
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
