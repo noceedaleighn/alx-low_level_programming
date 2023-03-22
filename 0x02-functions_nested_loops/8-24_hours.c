c3baf3ec0d6defd73d6cb4a81450ef3bf87c4452
noceedaleighn <no.ceepho06@gmail.com>
2023-03-22 01:29:41 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,23 +6,24 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; b <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					if (a >= 2 && b >= 4)
-						break;
+					for (d = 0; b <= 9; d++)
+					{
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
 					_putchar(c + 48);
 					_putchar(d + 48);
 					_putchar('\n');
+					}
 				}
 			}
 		}
